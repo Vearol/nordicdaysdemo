ed1a0b03984f08243c0c33f974d2e81dcbbc60aa
EUROPE\likushni <likushni@microsoft.com>
2023-11-06 20:26:48 +0200

    fix init

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,26 +1,19 @@
-variable "client_secret" {
-}
-
-provider "azurerm" {
-  features {}
-
-  client_id       = "80048ea6-2968-4870-9a4e-8f23d163696b"
-  tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-  subscription_id = "fa0c1c72-d987-4fda-a66c-9dcf889f50a9"
-}
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~>3.0"
     }
   }
-
   backend "azurerm" {
-    resource_group_name  = "nordic-days-demo-terra"
-    storage_account_name = "nordicdemostorageterra"
-    container_name       = "tfstate"
-    key                  = "terraform-base.tfstate"
+      resource_group_name  = "nordic-days-demo-terra"
+      storage_account_name = "nordicdemostorageterra"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
   }
+
+}
+
+provider "azurerm" {
+  features {}
 }
