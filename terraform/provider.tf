6aaf86dd41cea0f28266c8b7e90a654f41ef76b5
EUROPE\likushni <likushni@microsoft.com>
2023-11-06 19:32:59 +0200

    change init

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,8 +4,8 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.STATE_STORAGE_ACCOUNT_NAME
-    storage_account_name = var.STATE_STORAGE_ACCOUNT_NAME
+    resource_group_name  = "nordic-days-demo-terra"
+    storage_account_name = "nordicdemostorageterra"
     container_name       = "tfstate"
     key                  = "terraform-base.tfstate"
   }
