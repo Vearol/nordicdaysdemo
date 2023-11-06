a2ab838acba2657c08929792d57a5fab2793a4d1
EUROPE\likushni <likushni@microsoft.com>
2023-11-06 20:14:21 +0200

    add terraform azure login

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,13 @@
+variable "client_secret" {
+}
+
 provider "azurerm" {
   features {}
+
+  client_id       = "80048ea6-2968-4870-9a4e-8f23d163696b"
+  client_secret   = var.client_secret
+  tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+  subscription_id = "fa0c1c72-d987-4fda-a66c-9dcf889f50a9"
 }
 
 terraform {
