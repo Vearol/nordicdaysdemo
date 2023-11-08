eb0b32617e371cad79d3e95bf1a0bb15281112a4
EUROPE\likushni <likushni@microsoft.com>
2023-11-08 12:39:04 +0200

    refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "nordicdays-terra-state"
+      storage_account_name = "nordicdaysterrastate"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+      use_oidc             = true
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
 data "azurerm_client_config" "current" {}
 
 # ----------------------- Resource Group ------------------------ 
